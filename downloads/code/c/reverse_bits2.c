313aa4abfd66b5c05805003332fc9597bfdf3d78
Bill Smartt <bsmartt13@gmail.com>
2012-01-17 07:39:01 -0800

    Site updated at 2012-01-17 15:39:01 UTC

--- downloads/code/c/reverse_bits2.c
+++ downloads/code/c/reverse_bits2.c
@@ -8,11 +8,11 @@ int main(int argc, char **argv)
 	unsigned int x = 0x01234567, k = REVERSE_BITS;
 	
 	/* note: the order of these assignment statements is irrelevant. */
-	x = if(k & 1) { (x & 0x55555555) << 1  | (x & 0xAAAAAAAA) >> 1; }
-	x = if(k & 2) { (x & 0x33333333) << 2  | (x & 0xCCCCCCCC) >> 2; }
-	x = if(k & 4) { (x & 0x0f0f0f0f) << 4  | (x & 0xf0f0f0f0) >> 4; }
-	x = if(k & 8) { (x & 0x00ff00ff) << 8  | (x & 0xff00ff00) >> 8; }
-	x = if(k & 16) { (x & 0x0000ffff) << 16 | (x & 0xffff0000) >> 16; }
+	if(k & 1)  x = { (x & 0x55555555) << 1  | (x & 0xAAAAAAAA) >> 1; }
+	if(k & 2)  x = { (x & 0x33333333) << 2  | (x & 0xCCCCCCCC) >> 2; }
+	if(k & 4)  x = { (x & 0x0f0f0f0f) << 4  | (x & 0xf0f0f0f0) >> 4; }
+	if(k & 8)  x = { (x & 0x00ff00ff) << 8  | (x & 0xff00ff00) >> 8; }
+	if(k & 16) x = { (x &           ) << 16 | (x &           ) >> 16; }
 	
 	return 0;
 }
